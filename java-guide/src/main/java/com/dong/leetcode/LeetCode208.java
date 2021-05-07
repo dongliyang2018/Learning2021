d78f07f6dbd77be409b79c5b65a2b2350d637b2e
dongliyang <dongliyang2008@gmail.com>
2021-05-07 22:20:18 +0800

    前缀树

---
+++ java-guide/src/main/java/com/dong/leetcode/LeetCode208.java
@@ -0,0 +1,69 @@
+package com.dong.leetcode;
+
+/**
+ * 前缀树
+ *
+ * @version 1.0 2021/5/7
+ * @author dongliyang
+ */
+public class LeetCode208 {
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+        trie.insert("apple");
+        System.out.println("trie.search(\"apple\") = " + trie.search("apple"));
+        System.out.println("trie.search(\"app\") = " + trie.search("app"));
+        System.out.println("trie.startsWith(\"app\") = " + trie.startsWith("app"));
+        trie.insert("app");
+        System.out.println("trie.search(\"app\") = " + trie.search("app"));
+    }
+}
+
+class Trie {
+
+    /** 用于存放26个字母 */
+    private Trie[] children;
+    /** 是否是一个单词的结尾 */
+    private boolean isEnd;
+
+    public Trie() {
+        children = new Trie[26];
+        isEnd = false;
+    }
+
+    public void insert(String word) {
+        Trie node = this;
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            int index = ch - 'a';
+            if (node.children[index] == null) {
+                node.children[index] = new Trie();
+            }
+            node = node.children[index];
+        }
+        node.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        Trie node = searchPrefix(word);
+        return node != null && node.isEnd;
+    }
+
+    public boolean startsWith(String prefix) {
+        return searchPrefix(prefix) != null;
+    }
+
+    private Trie searchPrefix(String prefix) {
+        Trie node = this;
+        for (int i = 0; i < prefix.length(); i++) {
+            char ch = prefix.charAt(i);
+            int index = ch - 'a';
+            if (node.children[index] == null) {
+                return null;
+            }
+            node = node.children[index];
+        }
+        return node;
+    }
+}
+
+
